5ad08a84c3425748af33cb6901ae8ee58ae098f0
Jessbpl <jessbrapl@gmail.com>
2024-06-12 01:07:58 +0200

    set up search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>My Dictionary</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Cooded by Jessica Bravo</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
