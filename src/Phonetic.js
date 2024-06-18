d43ef2ff7b78635273658f15ff4f8d16aef78ccf
Jessbpl <jessbrapl@gmail.com>
2024-06-18 23:14:29 +0200

    added phonetic component

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+  if (props.phonetic) {
+    return (
+      <div className="Phonetic">
+        <h2>{props.phonetic}</h2>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
