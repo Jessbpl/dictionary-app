de5f217110327b3e8b3899dca32d376bfc5562dc
Jessbpl <jessbrapl@gmail.com>
2024-06-21 00:30:22 +0200

    set default keyword to display on first loading and updated style

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ export default function App() {
       <div className="container">
         <header className="App-header">My dictionary</header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="life" />
         </main>
         <footer className="App-footer">Cooded by Jessica Bravo</footer>
       </div>
     </div>
   );
 }
+//What we do is to work on the <Dictionary>, so we define the defaultKeyword="life" and then we go to the Dictionary component and we say that function Dictionary (props) is going to have acces to props and then we change the state let [keyword, setKeyword]=useState(props.defaultKeyword); because that what we are sending from the outside component App.js.
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,41 +3,61 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyWord, setKeyWord] = useState("");
-  let [results, setResults] = useState("");
-
+export default function Dictionary(props) {
+  let [keyWord, setKeyWord] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  //I want to have a boolean to track if the page has been loaded or not. Its going to have a false by default bc the page hasn´t been loaded the first time  that has been rendered. So now we have to create a condition
+  let [loaded, setLoaded] = useState(false);
   function handleResponse(response) {
-    console.log(response.data);
-    console.log(response.data.meanings.definition);
-
     setResults(response.data);
   }
 
-  function search(event) {
-    //documentation https://www.shecodes.io/learn/apis/dictionary
-    console.log(event);
-    setKeyWord(event.target.value);
-  }
-  function handleKeyWordUpdate(event) {
-    event.preventDefault();
+  function search() {
     const apiKey = `eb214ccaa33987f7248o49846e082tab`;
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
-    console.log(apiUrl);
+    // console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={handleKeyWordUpdate}>
-        <input
-          type="search"
-          autoFocus={true}
-          placeholder="Type a word"
-          onChange={search}
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
+    //documentation https://www.shecodes.io/learn/apis/dictionary
+    // console.log(event);
+    setKeyWord(event.target.value);
+  }
+
+  //what loaded (at the bottom) is going to do is setLoaded to true (so now we can say it´s been loaded) but also is going to search
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  //if it has been loaded i want to show what´s inside the return
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            autoFocus={true}
+            placeholder="Please type a word"
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <div className="hint">Example: friendship, joke, wish... </div>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    //but if hasn´t been loaded yet if (it is the first time this page is being rendered) and i am going to return "Loading..."
+    load();
+    return "Loading";
+  }
 }
+
+//Ideally, we want the page to load a word when the dictionary loads. I want the dictionary to load something on default. If i create a new state (loaded, setLoaded) to track if it has loaded something or not (it´s the first time it has loaded something), bc loaded by default is false. So then what i do is i say if its loaded show the form but if not i want to call the function Load wich is changing the state to true but i also wan to search so im calling search but not receiving results. now i need a new keyword (on the keyword state) so this word is being loaded by default.
+//A better approach is to send all of this from an outside component. So now we are going to work on the app.js component
