8c279344c39f8c0731f99090b17e5555099b0903
Jessbpl <jessbrapl@gmail.com>
2024-06-27 00:20:21 +0200

    added audio player for pronuntiation

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./Phonetic.css";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
-  if (props.phonetic) {
-    return (
-      <div className="Phonetic">
-        <h2>{props.phonetic}</h2>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  let url = `https://ssl.gstatic.com/dictionary/static/sounds/20200429/${props.word}--_gb_1.mp3`;
+  return (
+    <div className="Phonetic">
+      <h2> /{props.phonetic}/ </h2>
+      <ReactAudioPlayer src={url} autoPlay controls />
+    </div>
+  );
 }
--- src/Results.js
+++ src/Results.js
@@ -7,10 +7,11 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2 className="word">{props.results.word}</h2>
-        <p>
-          <Phonetic phonetic={props.results.phonetic} />
-        </p>
+        <h2>{props.results.word}</h2>
+        <Phonetic
+          phonetic={props.results.phonetic}
+          word={props.results.word}
+        />{" "}
         <br />
         {props.results.meanings.map(function (meaning, index) {
           return (
